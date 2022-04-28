c56f36466e462ea0d3b427c866308c746ed95f36
Fenroe <scrambles9494@gmail.com>
2022-04-28 18:25:36 +0300

    added firebase file

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAE3bZ1edrWid46Dqf6FQyS4Je5WZNphp8',
+  authDomain: 'wimby-a6cb4.firebaseapp.com',
+  projectId: 'wimby-a6cb4',
+  storageBucket: 'wimby-a6cb4.appspot.com',
+  messagingSenderId: '870472932974',
+  appId: '1:870472932974:web:d994bee3d178c5a27a907f',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
