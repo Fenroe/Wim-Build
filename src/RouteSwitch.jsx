1781f0560b36a3f9c9a8cd1f12904faff21ef5c7
Fenroe <scrambles9494@gmail.com>
2022-05-02 21:17:34 +0300

    hosting project

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -6,7 +6,7 @@ import About from './About';
 
 function RouteSwitch() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
